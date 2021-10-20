d75b6cfdceba4bf179289a530a9741fc1e5e2b59
Rick Nijhuis <rick.r.nijhuis@gmail.com>
2021-10-20 21:22:15 +0200

    Add project files.

---
+++ RGEngine.Logging/includes/logger.h
@@ -0,0 +1,116 @@
+﻿#pragma once
+
+#pragma once
+
+#include "core/error_codes.h"
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdint.h>
+
+struct logger_event
+{
+	va_list ap;
+	const char* fmt;
+	const char* file;
+	struct tm* time;
+	void* udata;
+	int line;
+	int level;
+};
+
+typedef void (*logger_log_fn)(struct logger_event* event);
+typedef void (*logger_lock_fn)(bool should_lock, void* data);
+
+enum log_levels { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
+
+/// <summary>
+/// gets the log level name of specified level
+/// </summary>
+/// <param name="level">: to get the name from</param>
+/// <returns>pointer to initialize</returns>
+const char* logger_level_string(enum log_levels level);
+
+/// <summary>
+/// sets a lock function to lock specified data when calling from multiple threads
+/// </summary>
+/// <param name="callback">: function, called to lock specified data when logging</param>
+/// <param name="data">: to lock, passed to callback when called</param>
+void logger_set_lock(logger_lock_fn callback, void* data);
+
+/// <summary>
+/// sets log level, any logging call with this or higher level will be logged, anything lower will be ignored
+/// </summary>
+/// <param name="level">: to set</param>
+void logger_set_level(enum log_levels level);
+
+/// <summary>
+/// if quiet prevents logging to stderr
+/// </summary>
+/// <param name="enable">: quiet or not</param>
+void logger_set_quiet(bool enable);
+
+/// <summary>
+/// adds callback to be called when logging
+/// </summary>
+/// <param name="callback">: function to be called</param>
+/// <param name="data">:  to log, passed to callback</param>
+/// <param name="level">:  of when to call the callback</param>
+/// <returns>error code or SUCCESS</returns>
+enum error_codes logger_add_callback(logger_log_fn callback, void* data, enum log_levels level);
+
+/// <summary>
+/// adds file to log to when logging
+/// </summary>
+/// <param name="file">: pointer to log to</param>
+/// <param name="level">:  of when to log to the file</param>
+/// <returns>error code or SUCCESS</returns>
+enum error_codes logger_add_file(FILE* file, enum log_levels level);
+
+/// <summary>
+/// log function, calls all callbacks and logs to stdout
+/// </summary>
+/// <param name="level">: of when to call the callback</param>
+/// <param name="file">: name of log origin</param>
+/// <param name="line">: number of log origin</param>
+/// <param name="fmt">: log string</param>
+/// <param name="...">: variadic parameter to pass to formated string</param>
+void logger_log(enum log_levels level, const char* file, int32_t line, const char* fmt, ...);
+
+/// <summary>
+/// utility log macro calls log_log with predefined level, file and line
+/// </summary>
+/// <param name="...">: variadic parameter to pass log string and values to pass to log string</param>
+#define log_trace(...) logger_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
+
+/// <summary>
+/// utility log macro calls log_log with predefined level, file and line
+/// </summary>
+/// <param name="...">: variadic parameter to pass log string and values to pass to log string</param>
+#define log_debug(...) logger_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+
+/// <summary>
+/// utility log macro calls log_log with predefined level, file and line
+/// </summary>
+/// <param name="...">: variadic parameter to pass log string and values to pass to log string</param>
+#define log_info(...)  logger_log(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
+
+/// <summary>
+/// utility log macro calls log_log with predefined level, file and line
+/// </summary>
+/// <param name="...">: variadic parameter to pass log string and values to pass to log string</param>
+#define log_warn(...)  logger_log(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
+
+/// <summary>
+/// utility log macro calls log_log with predefined level, file and line
+/// </summary>
+/// <param name="...">: variadic parameter to pass log string and values to pass to log string</param>
+#define log_error(...) logger_log(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+
+/// <summary>
+/// utility log macro calls log_log with predefined level, file and line
+/// </summary>
+/// <param name="...">: variadic parameter to pass log string and values to pass to log string</param>
+#define log_fatal(...) logger_log(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
---
+++ RGEngine.Logging/src/logger.c
@@ -0,0 +1,153 @@
+﻿#include "logger.h"
+
+#define MAX_CALLBACKS 32
+
+struct callback
+{
+	logger_log_fn fn;
+	void* udata;
+	enum log_levels level;
+};
+
+static struct
+{
+	void* udata;
+	logger_lock_fn lock;
+	enum log_levels level;
+	bool quiet;
+	struct callback callbacks[MAX_CALLBACKS];
+} L;
+
+static const char* level_strings[] = {
+  "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"
+};
+
+#ifdef LOGGER_USE_COLOR
+static const char* level_colors[] = {
+  "\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"
+};
+#endif
+
+static void stdout_callback(struct logger_event* event)
+{
+	char buf[16];
+	buf[strftime(buf, sizeof(buf), "%H:%M:%S", event->time)] = '\0';
+#ifdef LOG_USE_COLOR
+	fprintf(
+		event->udata, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
+		buf, level_colors[event->level], level_strings[event->level],
+		event->file, event->line);
+#else
+	fprintf(
+		event->udata, "%s %-5s %s:%d: ",
+		buf, level_strings[event->level], event->file, event->line);
+#endif
+	vfprintf(event->udata, event->fmt, event->ap);
+	fprintf(event->udata, "\n");
+	fflush(event->udata);
+}
+
+static void file_callback(struct logger_event* event)
+{
+	char buf[64];
+	buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", event->time)] = '\0';
+	fprintf(
+		event->udata, "%s %-5s %s:%d: ",
+		buf, level_strings[event->level], event->file, event->line);
+	vfprintf(event->udata, event->fmt, event->ap);
+	fprintf(event->udata, "\n");
+	fflush(event->udata);
+}
+
+static void lock(void)
+{
+	if (L.lock) { L.lock(true, L.udata); }
+}
+
+static void unlock(void)
+{
+	if (L.lock) { L.lock(false, L.udata); }
+}
+
+const char* logger_level_string(enum log_levels level)
+{
+	return level_strings[level];
+}
+
+void logger_set_lock(logger_lock_fn fn, void* udata)
+{
+	L.lock = fn;
+	L.udata = udata;
+}
+
+void logger_set_level(enum log_levels level)
+{
+	L.level = level;
+}
+
+void logger_set_quiet(bool enable)
+{
+	L.quiet = enable;
+}
+
+enum error_codes log_add_callback(logger_log_fn callback, void* data, enum log_levels level)
+{
+	for (int i = 0; i < MAX_CALLBACKS; i++)
+	{
+		if (!L.callbacks[i].fn)
+		{
+			L.callbacks[i] = (struct callback){ callback, data, level };
+			return SUCCESS;
+		}
+	}
+	return ERROR_MAX_LOG_CALLBACKS;
+}
+
+enum error_codes logger_add_file(FILE* fp, enum log_levels level)
+{
+	return logger_add_callback(file_callback, fp, level);
+}
+
+static void init_event(struct logger_event* event, void* udata)
+{
+	if (!event->time)
+	{
+		time_t t = time(NULL);
+		event->time = localtime(&t);
+	}
+	event->udata = udata;
+}
+
+void logger_log(enum log_levels level, const char* file, int32_t line, const char* fmt, ...)
+{
+	struct logger_event event = {
+	  .fmt = fmt,
+	  .file = file,
+	  .line = line,
+	  .level = level,
+	};
+
+	lock();
+
+	if (!L.quiet && level >= L.level)
+	{
+		init_event(&event, stderr);
+		va_start(event.ap, fmt);
+		stdout_callback(&event);
+		va_end(event.ap);
+	}
+
+	for (int i = 0; i < MAX_CALLBACKS && L.callbacks[i].fn; i++)
+	{
+		struct callback* cb = &L.callbacks[i];
+		if (level >= cb->level)
+		{
+			init_event(&event, cb->udata);
+			va_start(event.ap, fmt);
+			cb->fn(&event);
+			va_end(event.ap);
+		}
+	}
+
+	unlock();
+}
